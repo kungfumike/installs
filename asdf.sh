b7f5fb5be24fde0e0b93064dfb5399d046771b8e
Mike Thompson <michael.p.thompson@gmail.com>
2016-04-28 12:28:53 -0700

    Not going with Stringswan, asdf for Erlang/Elixir

---
+++ asdf.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+echo '. $HOME/.asdf/asdf.sh' >> ~/.zshrc
+echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
+sudo apt-get install automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev
--- strongswan.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install automake autoconf libtool pkg-config gettext flex bison gperf
-git clone git://git.strongswan.org/strongswan.git
-./autogen.sh
-./configure
